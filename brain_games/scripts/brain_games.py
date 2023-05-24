509bd7b6f2ab3c84c69afd93ca63f69774abbbfa
Ukhlina Ekaterina <adelaida_19@mail.ru>
2023-05-24 16:47:56 +0300

    add cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
+def welcome():
+    print('Welcome to the Brain Games!')
 
 def main():
-    print('Welcome to the Brain Games!')
+    welcome()
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
