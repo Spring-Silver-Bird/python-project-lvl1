fb71e5d475b6e57d45922a420ce86ed42c25ac7c
Ukhlina Ekaterina <adelaida_19@mail.ru>
2023-05-30 17:06:08 +0300

    add file for brain-even game

---
+++ brain_games/games/even.py
@@ -0,0 +1,29 @@
+from random import randint
+import prompt
+
+rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+LIFES = 3
+
+#Проверка на четность
+def is_even(number):
+    return number % 2 == 0
+
+
+#Описание логики игры
+#Функция возвращает вопрос и правильный ответ
+def game():
+    min_number = 1 #Минимальное рандомное число
+    max_number = 100 #Максимальное рандомное число
+    random_number = randint(min_number, max_number)
+    #print('Question:', random_number)
+    #user_answer = prompt.string('Your answer: ')
+
+    if is_even(random_number):
+        corect_answer = 'yes'
+    else:
+        corect_answer = 'no'
+
+    question = str(random_number)
+    return question, corect_answer
+
+
